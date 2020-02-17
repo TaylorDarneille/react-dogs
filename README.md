e0955d006b483316f6fb88a5433c563d220459cd
Taylor Darneille <tmdarneille@gmail.com>
2020-02-16 19:33:41 -0800

    'get dogs' commit without the CORS stuff since I don't have the API and can't test it

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ React environment variables
 https://create-react-app.dev/docs/adding-custom-environment-variables/
 
 note: you will need to stop and restart the 'npm start' create-react-app development environment/server to see this change
+
--- src/DogContainer/index.js
+++ src/DogContainer/index.js
@@ -13,6 +13,27 @@ class DogContainer extends Component {
 		}
 	}
 
+	// component lifecycle methods should always be defined as class methods
+	componentDidMount() {
+		// get the dogs when the component mounts
+		this.getDogs()
+	}
+
+	// the word "async" before a function means one simple thing: a function always returns a promise. Other values are wrapped in a resolved promise automatically.
+	// more on this: https://javascript.info/async-await
+	getDogs = async () => {
+		try {
+			// the keyword await makes JS wait until that promist settles and returns its result
+			const dogsResponse = await fetch(process.env.REACT_APP_API_URL + "/api/v1/dogs")
+			const dogsJson = await dogsResponse.json()
+			console.log("here is the data we got in getDogs in DogContainer");
+			console.log(dogsJson);	
+		} catch(err) {
+			// catches errors both in fetch and in response.json
+			console.log(err)
+		}
+	}
+
 	render() {
 		return(
 			"Dog Container"
