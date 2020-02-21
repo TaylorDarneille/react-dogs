9854be6d28135841af1715de0039ad0b70b3ddc3
Taylor Darneille <tmdarneille@gmail.com>
2020-02-21 11:18:01 -0800

    implemented semantic-ui

--- src/DogList/index.js
+++ src/DogList/index.js
@@ -3,11 +3,28 @@ import { Card } from 'semantic-ui-react'
 
 function DogList(props) {
 	const dogs = props.dogs.map((dog) => {
-		return <li key={dog.id}>{dog.name} is a {dog.breed} that belongs to {dog.owner}</li>
+		let colors = ["red","orange","yellow","olive","green","teal","blue","violet","purple","pink","brown","grey","black"]
+		let color = colors[Math.floor(Math.random()*colors.length)-1]
+		console.log("color:", color);
+		return (
+			<Card key={dog.id} centered={true} color={color} >
+				<Card.Content>
+					<Card.Header>
+						{dog.name}
+					</Card.Header>
+				</Card.Content>
+				<Card.Description>
+					{dog.name} is a {dog.breed} that belongs to {dog.owner}
+				</Card.Description>
+			</Card>
+		)
+		// return <li key={dog.id}>{dog.name} is a {dog.breed} that belongs to {dog.owner}</li>
 	})
 	return (
 		<ul>
-			{dogs}
+			<Card.Group>
+				{dogs}
+			</Card.Group>
 		</ul>
 	)
 }
